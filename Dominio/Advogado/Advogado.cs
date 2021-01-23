d3e82c49adc20da66f0cebc090ce06e1dc022102
Pablo <pablo952014@gmail.com>
2021-01-23 20:52:08 -0300

    Implementação de validação no dominio para inclusão e alteração de Entidades

--- Dominio/Advogado/Advogado.cs
+++ Dominio/Advogado/Advogado.cs
@@ -7,8 +7,40 @@ namespace Dominio.Advogado
 {
     public class Advogado : EntidadeBase
     {
-        public string Nome { get; set; }
-        public string Senioridade { get; set; }
-        public virtual IEnumerable<Endereco.Endereco> Enderecos { get; set;  }
+        public string Nome { get;private set; }
+        public string Senioridade { get;private set; }
+        public virtual IEnumerable<Endereco.Endereco> Enderecos { get;private set;  }
+
+        protected Advogado()
+        {
+
+        }
+
+        public Advogado(string pStrNome, string pStrSenioridade)
+        {
+            ValidarParaInclusao(pStrNome, pStrSenioridade);
+            Nome = pStrNome;
+            Senioridade = pStrSenioridade;
+        }
+        public Advogado(int pIntId, string pStrNome, string pStrSenioridade)
+        {
+            ValidarParaAlteracao(pIntId, pStrNome, pStrSenioridade);
+            Id = pIntId;
+            Nome = pStrNome;
+            Senioridade = pStrSenioridade;
+        }
+
+        private void ValidarParaInclusao(string pStrNome, string pStrSenioridade)
+        {
+            if (string.IsNullOrEmpty(pStrNome) || pStrNome.Length > 50) throw new ArgumentException("Nome de cliente inválido !");
+            if (string.IsNullOrEmpty(pStrSenioridade) || pStrSenioridade.Length > 40) throw new ArgumentException("Senioridade inválida !");
+        }
+
+        private void ValidarParaAlteracao(int pIntId, string pStrNome, string pStrSenioridade)
+        {
+            if (pIntId == 0) throw new ArgumentException("Id inválido !");
+            if (string.IsNullOrEmpty(pStrNome) || pStrNome.Length > 50) throw new ArgumentException("Nome de cliente inválido !");
+            if (string.IsNullOrEmpty(pStrSenioridade) || pStrSenioridade.Length > 40) throw new ArgumentException("Senioridade inválida !");
+        }
     }
 }
--- Dominio/Endereco/Endereco.cs
+++ Dominio/Endereco/Endereco.cs
@@ -7,11 +7,56 @@ namespace Dominio.Endereco
 {
     public class Endereco : EntidadeBase
     {
-        public string Logradouro { get; set; }
-        public string Bairro { get; set; }
-        public string Cidade { get; set; }
-        public string Estado { get; set; }
-        public int AdvogadoId { get; set; }
+        public string Logradouro { get;private  set; }
+        public string Bairro { get;private  set; }
+        public string Cidade { get;private  set; }
+        public string Estado { get;private  set; }
+        public int AdvogadoId { get;private  set; }
         public virtual Advogado.Advogado Advogado { get; set; }
+
+        protected Endereco()
+        {
+
+        }
+
+        public Endereco(string pStrLogradouro, string pStrBairro, string pStrCidade, string pStrEstado, int pIntAdvogadoId)
+        {
+            ValidarParaInclusao(pStrLogradouro, pStrBairro, pStrCidade, pStrEstado, pIntAdvogadoId);
+            Logradouro = pStrLogradouro;
+            Bairro = pStrBairro;
+            Cidade = pStrCidade;
+            Estado = pStrEstado;
+            AdvogadoId = pIntAdvogadoId;
+        }
+
+        public Endereco(int pIntId, string pStrLogradouro, string pStrBairro, string pStrCidade, string pStrEstado, int pIntAdvogadoId)
+        {
+            ValidarParaAlteracao(pIntId, pStrLogradouro, pStrBairro, pStrCidade, pStrEstado, pIntAdvogadoId);
+            Id = pIntId;
+            Logradouro = pStrLogradouro;
+            Bairro = pStrBairro;
+            Cidade = pStrCidade;
+            Estado = pStrEstado;
+            AdvogadoId = pIntAdvogadoId;
+        }
+
+        private void ValidarParaInclusao(string pStrLogradouro, string pStrBairro, string pStrCidade, string pStrEstado, int pIntAdvogadoId)
+        {
+            if (string.IsNullOrEmpty(pStrLogradouro) || pStrLogradouro.Length > 50) throw new ArgumentException("Logradouro Inválido !");
+            if (string.IsNullOrEmpty(pStrBairro) || pStrBairro.Length > 50) throw new ArgumentException("Bairro Inválido !");
+            if (string.IsNullOrEmpty(pStrCidade) || pStrCidade.Length > 40) throw new ArgumentException("cidade Inválido !");
+            if (string.IsNullOrEmpty(pStrEstado) || pStrEstado.Length > 40) throw new ArgumentException("Estado Inválido !");
+            if (pIntAdvogadoId == 0) throw new ArgumentException("Informe um advogado para poder cadastrar um Endereço !");
+        }
+
+        private void ValidarParaAlteracao(int pIntId, string pStrLogradouro, string pStrBairro, string pStrCidade, string pStrEstado, int pIntAdvogadoId)
+        {
+            if (pIntId == 0) throw new ArgumentException("Informe um id de Endereço !");
+            if (string.IsNullOrEmpty(pStrLogradouro) || pStrLogradouro.Length > 50) throw new ArgumentException("Logradouro Inválido !");
+            if (string.IsNullOrEmpty(pStrBairro) || pStrBairro.Length > 50) throw new ArgumentException("Bairro Inválido !");
+            if (string.IsNullOrEmpty(pStrCidade) || pStrCidade.Length > 40) throw new ArgumentException("cidade Inválido !");
+            if (string.IsNullOrEmpty(pStrEstado) || pStrEstado.Length > 40) throw new ArgumentException("Estado Inválido !");
+            if (pIntAdvogadoId == 0) throw new ArgumentException("Informe um advogado para poder cadastrar um Endereço !");
+        }
     }
 }
